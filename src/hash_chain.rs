7aa4a2704299a8fa4872f90836dac864dcf24222
littleblack111 <littleblack11111@gmail.com>
2025-10-22 14:55:28 +0800

    chore(hashchain): rename hash

--- src/hash_chain.rs
+++ src/hash_chain.rs
@@ -35,7 +35,7 @@ impl<T: Encode> HashChain<T> {
             })
             .unwrap_or_default();
         self.data
-            .push((Self::with_hash((&prev_hash, &data)), data));
+            .push((Self::hash_item((&prev_hash, &data)), data));
     }
 
     // TODO: make try_last that does same as this but returns Err() if it's not the
@@ -47,7 +47,7 @@ impl<T: Encode> HashChain<T> {
             .position(|x| x.0 == *base_hash)
             .unwrap();
         self.data
-            .insert(pos + 1, (Self::with_hash((base_hash, &data)), data));
+            .insert(pos + 1, (Self::hash_item((base_hash, &data)), data));
 
         // everything after that hash is invalidated
         self.rehash(&base_hash);
@@ -63,7 +63,7 @@ impl<T: Encode> HashChain<T> {
             .iter()
             .enumerate()
             .all(|(prev_hash, (pred_prev_hash, _))| {
-                let prev_hash = Self::with_hash({
+                let prev_hash = Self::hash_item({
                     let prev_hash = &self.data[prev_hash - 1];
                     (&prev_hash.0, &prev_hash.1)
                 });
@@ -82,7 +82,7 @@ impl<T: Encode> HashChain<T> {
             .data
             .len())
             .map(|i| {
-                Self::with_hash({
+                Self::hash_item({
                     let prev_hash = &self.data[i - 1];
                     (&prev_hash.0, &prev_hash.1)
                 })
@@ -100,23 +100,12 @@ impl<T: Encode> HashChain<T> {
             });
     }
 
-    fn with_hash(data: (&Vec<u8>, &T)) -> Vec<u8> {
+    fn hash_item(data: (&Vec<u8>, &T)) -> Vec<u8> {
         let mut hasher = Sha256::new();
         hasher.update(bincode::encode_to_vec(data, bincode::config::standard()).unwrap());
         hasher
             .finalize()
             .to_vec()
-    }
-
-    fn hash_last(&self) -> Vec<u8> {
-        Self::with_hash({
-            let last = self
-                .data
-                .iter()
-                .last()
-                .unwrap();
-            (&last.0, &last.1)
-        })
     }
 }
 
